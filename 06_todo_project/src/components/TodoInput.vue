4731199873ed77b4cea54b7f7222635cab783add
ITKOO <itkoo2000@gmail.com>
2020-02-03 17:07:57 +0900

    06 실전어플리케이션 만들기 - TodoInpui 컴포넌트 구현

--- 06_todo_project/src/components/TodoInput.vue
+++ 06_todo_project/src/components/TodoInput.vue
@@ -1,13 +1,61 @@
 <template>
-  <div>input</div>
+  <div class="inputBox shadow">
+      <input type="text" v-model="newTodoItem" placeholder="Type what you have to do"
+      v-on:keyup.enter="addTodo">
+      <!-- 추가 버튼 -->
+      <span class="addContainer" v-on:click="addTodo">
+        <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+      </span>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  data(){
+    return{
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo(){
+      // 빈값 체크
+      if(this.newTodoItem !== ""){
+        var value = this.newTodoItem && this.newTodoItem.trim();  
+        // 로컬스토리지에 입력한 값 저장(키, 벨류)    
+        localStorage.setItem(this.newTodoItem, this.newTodoItem);
+        this.clearInput();
+      } 
+    },
+    clearInput(){
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  input:focus{
+    outline: none;
+  }
+  .inputBox{
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input{
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer{
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: inline-block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn{
+    color: white;
+    vertical-align: middle;
+  }
 </style>
